fe46a6fae477fe26aff9d32c2f99ef07c29e3bda
Nekmo <contacto@nekmo.com>
2015-10-26 17:58:20 +0100

    Update count-to.js

--- src/count-to.js
+++ src/count-to.js
@@ -6,7 +6,8 @@ var countTo = angular.module('countTo', [])
             link: function (scope, element, attrs) {
 
                 var e = element[0];
-                var num, refreshInterval, duration, steps, step, countTo, value, increment, precision;
+                var num, refreshInterval, duration, steps, step, countTo, value, increment, precision,
+                    precisionZero;
 
                 var calculate = function () {
                     refreshInterval = 30;
@@ -14,18 +15,24 @@ var countTo = angular.module('countTo', [])
                     scope.timoutId = null;
                     precision = parseInt(attrs.precision) || 0;
                     countTo = parseFloat(attrs.countTo) || 0;
+                    if(countTo % 1 == 0 && !(precisionZero)){
+                        precision = 0;
+                    }
                     scope.value = parseFloat(attrs.value, 10) || 0;
                     duration = (parseFloat(attrs.duration) * 1000) || 0;
 
                     steps = duration / refreshInterval;
                     increment = ((countTo - scope.value) / steps);
                     num = scope.value;
-                }
+                };
 
                 var tick = function () {
                     scope.timoutId = $timeout(function () {
                         num += increment;
                         step++;
+                        if(countTo % 1 == 0){
+                            precision = 0;
+                        }
                         if (step >= steps) {
                             $timeout.cancel(scope.timoutId);
                             num = countTo;
